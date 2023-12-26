861b611af5d6905db74a21a667a729bcb00f6e91
ductho05 <ductho23pro@gmail.com>
2023-12-26 17:40:00 +0700

    ductho: fix login fb

--- src/index.js
+++ src/index.js
@@ -5,11 +5,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import UserProvider from './stores/provider';
 import { DataProvider } from './stores/DataContext';
+import { CustomChat, FacebookProvider } from 'react-facebook'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <UserProvider>
         <DataProvider>
             <GlobalStyle>
+                <FacebookProvider appId="1325947341377754" chatSupport>
+                    <CustomChat pageId="198008766726901" minimized={true} />
+                </FacebookProvider>
                 <App />
             </GlobalStyle>
         </DataProvider>
