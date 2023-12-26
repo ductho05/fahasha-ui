9ec0bb5337441ac6bfe3985ee65e5786d7613053
ductho05 <ductho23pro@gmail.com>
2023-12-26 17:11:52 +0700

    ductho: fix login fb

--- src/components/FaceBookChatBox/FaceBookChatBox.js
+++ src/components/FaceBookChatBox/FaceBookChatBox.js
@@ -2,9 +2,12 @@ import React from 'react'
 import { CustomChat, FacebookProvider } from 'react-facebook'
 
 function FaceBookChatBox() {
+
+    console.log("dã vo")
+
     return (
         <FacebookProvider appId="1325947341377754" chatSupport>
-            <CustomChat pageId="198008766726901" minimized={true} />
+            <CustomChat pageId="198008766726901" minimized={false} />
         </FacebookProvider>
     )
 }
